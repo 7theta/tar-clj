79ee53e619bf10dcc05f8413354a9495bb4e0380
Achint Sandhu <sandhu@7theta.com>
2022-01-21 15:29:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# com.7theta/tar-clj
+
+[![Current Version](https://img.shields.io/clojars/v/com.7theta/tar-clj.svg)](https://clojars.org/com.7theta/tar-clj)
+[![GitHub license](https://img.shields.io/github/license/7theta/tar-clj.svg)](LICENSE)
+
+## Copyright and License
+
+Copyright © 2022 7theta
+
+Distributed under the MIT License.
+
---
+++ project.clj
@@ -0,0 +1,18 @@
+;;   Copyright (c) 7theta. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   MIT License (https://opensource.org/licenses/MIT) which can also be
+;;   found in the LICENSE file at the root of this distribution.
+;;
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any others, from this software.
+
+(defproject com.7theta/tar-clj "0.1.0"
+  :url "https://github.com/7theta/tar-clj"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies []
+                   :source-paths ["dev"]}}
+  :scm {:name "git"
+        :url "https://github.com/7theta/tar-clj"})
